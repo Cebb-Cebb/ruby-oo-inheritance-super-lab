ea03cfa8d8b7434be6f850f3172b1517493dd517
Propioception <github email address>
2020-12-07 14:55:48 -0500

    commit tree 8405f228d2724d932b578159b2a141809c66a3c2

---
+++ lib/student.rb
@@ -0,0 +1,15 @@
+class Student 
+    # extend ChattyStudent
+    # include ChattyStudent
+    # attr_accessor :student 
+
+    def hello
+     puts "Hey there! I'm so excited to learn stuff."
+    end 
+
+    def raise_hand
+      puts "Pick me!"
+    end 
+
+
+end 
